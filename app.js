cbbca4fee8bc0ff310b6e2e3a38ed39bcf2ed914
“Ankit <22amtics124@gmail.com>
2023-07-27 23:02:53 +0530

    hedder in dashboard

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require("body-parser");
 const user = require('./modals/User');
-const signinHandler = require('./controllers/_signinHandler');
-const signupHandler = require('./controllers/_signupHandler');
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, './templates'));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
