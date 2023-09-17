651158e35adeca4033ffacfcc3016c287388baf5
“Ankit <22amtics124@gmail.com>
2023-09-17 21:12:23 +0530

    update profile photo completed

--- routes/updateProfile.js
+++ routes/updateProfile.js
@@ -4,13 +4,13 @@ const updateProfileHandler = require('../controllers/_updateProfileHandler');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const sharp = require('sharp');
+// const sharp = require('sharp');
 
 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'F:/PROGRAMING/WEB DEVELOPMENT/SocietySync/uploads'); // Set the destination folder for uploaded files
+    cb(null, 'F:/PROGRAMING/WEB DEVELOPMENT/SocietySync/public/uploads'); // Set the destination folder for uploaded files
   },
   filename: function (req, file, cb) {
     const username = req.body.oldUsername;
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 
     console.log("file orig name");
     console.log(file.originalname);
-    cb(null, `${username}_profile${path.extname(file.originalname)}`); // Use the original filename
+    cb(null, `${username}_profile.jpg`); // Use the original filename
   },
 });
 
